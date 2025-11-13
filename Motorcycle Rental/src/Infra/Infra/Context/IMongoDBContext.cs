e4d60a821ead2f2b27dece8e547c52c4e9a20168
DavidBotelho <david.botelho@softwillians.com.br>
2025-11-13 20:24:01 -0300

    Criado Context.

---
+++ Motorcycle Rental/src/Infra/Infra/Context/IMongoDBContext.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Driver;
+
+namespace Infra.Context
+{
+    public interface IMongoDBContext
+    {
+        public IMongoDatabase DataBase { get; }
+
+        string GetConnectionStringFromEnvironment();
+    }
+}
