a92fa2aa399a77b196c96a60c2e7c92fc92e69e6
DavidBotelho <david.botelho@softwillians.com.br>
2025-11-13 21:28:18 -0300

    Criadas entidades Driver, Motorcycle e Rent

---
+++ Motorcycle Rental/src/Domain/Domain/Entities/Rent.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    public class Rent
+    {
+        public Guid Driver_id { get; set; }
+        public Guid Moto_id { get; set; }
+        public DateTime date_initio { get; set; }
+        public DateTime date_end { get; set; }
+        public DateTime date_forecast_end { get; set; }
+        public int plan { get; set; }
+    }
+}
